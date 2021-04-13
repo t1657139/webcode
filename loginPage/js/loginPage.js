cefd97806fdde1d3077ecca851cf28cf65565b58
t1657139 <48038474+t1657139@users.noreply.github.com>
2021-04-13 19:54:09 +0800

    Add files via upload

--- loginPage/js/loginPage.js
+++ loginPage/js/loginPage.js
@@ -10,3 +10,18 @@ Array.prototype.forEach.call(inputs, (input) => {
         }
     })
 });
+
+/* hover 更換圖片 */
+const buttons = document.getElementsByClassName("button");
+Array.prototype.forEach.call(buttons, (button) => {
+    const oldsrc = button.children[0].src;
+    const routes = oldsrc.split('/');
+    let newsrc;
+    button.addEventListener('mouseover', (e) => {
+        newsrc = "image/whenHover/" + routes[routes.length-1];
+        button.children[0].src = newsrc;
+    })
+    button.addEventListener('mouseout', (e) => {
+        button.children[0].src = oldsrc;
+    })
+})
